a7c02a80d1a915cc7bd1fdcc104a03c67e14e6c5
sizz-prog <ssi13@scasd.org>
2026-08-21 18:07:43 -0400

    you can drag the window

---
+++ script.js
@@ -0,0 +1,36 @@
+setInterval(function() {document.querySelector("#timeElement").innerHTML = new Date().toLocaleString()}, 1000);
+
+dragElement(document.getElementById("welcomeWindow"));
+function dragElement(element) {
+    var initialX = 0;
+    var initialY = 0;
+    var currentX = 0;
+    var currentY = 0;
+    if (document.getElementById(element.id + "Header")) {
+        document.getElementById(element.id + "Header").onmousedown = startDragging;
+    } else {
+        element.onmousedown = startDragging;
+    }
+    function startDragging(e) {
+        e = e || window.event;
+        e.preventDefault();
+        initialX = e.clientX;
+        initialY = e.clientY;
+        document.onmouseup = stopDragging;
+        document.onmousemove = dragElement;
+    }
+    function dragElement(e) {
+        e = e || window.event;
+        e.preventDefault();
+        currentX = initialX - e.clientX;
+        currentY = initialY - e.clientY;
+        initialX = e.clientX;
+        initialY = e.clientY;
+        element.style.top = (element.offsetTop - currentY) + "px";
+        element.style.left = (element.offsetLeft - currentX) + "px";
+    }
+    function stopDragging() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
