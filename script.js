591976a3429e576a417d70224d41c46b8a7f66c8
sizz-prog <ssi13@scasd.org>
2026-08-23 11:21:55 -0400

    scriptmaxxgooning

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 setInterval(function() {document.querySelector("#timeElement").innerHTML = new Date().toLocaleString()}, 1000);
 
-dragElement(document.getElementById("welcomeWindow"));
+/*dragElement(document.querySelector("#welcomeWindow"));
+dragElement(document.querySelector("#tsaWindow"));*/
 function dragElement(element) {
     var initialX = 0;
     var initialY = 0;
@@ -35,19 +36,77 @@ function dragElement(element) {
     }
 }
 
+function setupWindow(windowName) {
+    var window = document.querySelector("#" + windowName);
+    var windowClose = document.querySelector("#" + windowName + "Close");
+    var windowOpen = document.querySelector("#" + windowName + "Open");
+    windowClose.addEventListener("click", function() {
+        closeWindow(window);
+    });
+    windowOpen.addEventListener("click", function() {
+        openWindow(window);
+    });
+    addWindowTapHandling(window);
+    dragElement(window);
+}
 var welcomeWindow = document.querySelector("#welcomeWindow");
+var tsaWindow = document.querySelector("#tsaWindow");
+setupWindow("welcomeWindow");
+setupWindow("tsaWindow");
 function closeWindow(element) {
     element.style.display = "none";
 }
 function openWindow(element) {
-    element.style.display = "flex"
-}
+    element.style.display = "flex";
+    biggestIndex++;
+    element.style.zIndex = biggestIndex;
+    topBar.style.zIndex = biggestIndex + 1;
+}/*
 var welcomeWindowClose = document.querySelector("#welcomeWindowClose");
 var welcomeWindowOpen = document.querySelector("#welcomeWindowOpen");
+var tsaWindowClose = document.querySelector("#tsaWindowClose");
+var tsaWindowOpen = document.querySelector("#tsaIcon");
 welcomeWindowClose.addEventListener("click", function() {
     closeWindow(welcomeWindow);
 });
 welcomeWindowOpen.addEventListener("click", function() {
     openWindow(welcomeWindow);
 });
-  
+tsaWindowClose.addEventListener("click", function() {
+    closeWindow(tsaWindow);
+});
+tsaWindowOpen.addEventListener("click", function() {
+    openWindow(tsaWindow);
+});*/
+
+var selectedIcon = undefined;
+function selectIcon(element) {
+    element.classList.add("selected");
+    selectedIcon = element;
+}
+function deselectIcon(element) {
+    element.classList.remove("selected");
+    selectedIcon = undefined;
+}
+function handleIconTap(element) {
+    if (element.classList.contains("selected")) {
+        deselectIcon(element);
+        openWindow(element);
+    } else {
+        selectIcon(element);
+    }
+}
+
+var biggestIndex = 0;
+function addWindowTapHandling(element) {
+    element.addEventListener("mousedown", () =>
+        handleWindowTap(element)
+    )
+}
+function handleWindowTap(element) {
+    biggestIndex++;
+    element.style.zIndex = biggestIndex;
+    topBar.style.zIndex = biggestIndex + 1;
+    deselectIcon(selectedIcon)
+}
+var topBar = document.querySelector("#topBar");
